4340d71faac996902d743813c7c37d532aeac4d2
Steve Huckle <s.huckle@sussex.ac.uk>
2018-09-05 13:42:24 +0100

    Architecture stuff

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,20 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import { createBrowserHistory } from 'history'
+import { configureStore } from 'app/store'
+import { App } from './app'
 
-import { Hello } from "./components/hello";
+// prepare store
+const history = createBrowserHistory()
+const store = configureStore(history)
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
