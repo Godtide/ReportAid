9e6df9b8983bf314ac127ccb33d0c908f29dde4e
Steve Huckle <s.huckle@sussex.ac.uk>
2018-09-05 18:50:54 +0100

    Router Shmouter

--- app/src/app/store/store.ts
+++ app/src/app/store/store.ts
@@ -1,23 +1,47 @@
-import { Store, createStore, compose, applyMiddleware } from 'redux'
-import { routerMiddleware, connectRouter } from 'connected-react-router/immutable'
+import * as React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { RouteComponentProps } from 'react-router'
+import { routerMiddleware, connectRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import Immutable from 'immutable'
 
 import { logger } from '../middleware'
-import { RootState, rootReducer } from '../reducers'
 
+const history = createBrowserHistory()
+const initialState = Immutable.Map()
 
-export function configureStore(history: history, initialState?: RootState): Store<RootState> {
-  let middleware = applyMiddleware(logger, routerMiddleware(history))
+export namespace Store {
+  export interface Props extends RouteComponentProps<void> {
+    history: history,
+    initialState: initialState
+  }
+}
 
-  if (process.env.NODE_ENV !== 'production') {
-    middleware = composeWithDevTools(middleware);
+export class Store extends React.Component<Store.Props> {
+
+  constructor(props: Store.Props, context?: any) {
+    super(props, context)
   }
 
-  const store = createStore(
-    connectRouter(history)(rootReducer), // new root reducer with router state
-    initialState,
-    middleware
-  )
+  getHistory () {
+    return this.props.history
+  }
 
-  return store
+  configureStore () {
+
+    let middleware = applyMiddleware(logger, routerMiddleware(history))
+
+    if (process.env.NODE_ENV !== 'production') {
+      middleware = composeWithDevTools(middleware);
+    }
+
+    const store = createStore(
+      connectRouter(history)(rootReducer), // new root reducer with router state
+      initialState,
+      middleware
+    )
+
+    return store
+  }
 }
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,12 +3,10 @@ import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { App } from './app/containers/app'
-import { createBrowserHistory } from 'history'
-import { configureStore } from './app/store'
+import { Store } from './app/store'
 
-// prepare store
-const history = createBrowserHistory()
-const store = configureStore(history)
+const store = Store.configureStore()
+const history = Store.getHistory()
 
 ReactDOM.render(
   <Provider store={store}>
