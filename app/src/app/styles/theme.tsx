eac2b56cd4f5d39cc958102e9b94da717d1b4c7a
Steve Huckle <s.huckle@sussex.ac.uk>
2018-09-21 13:48:04 +0100

    Font size

--- app/src/app/styles/theme.tsx
+++ app/src/app/styles/theme.tsx
@@ -9,7 +9,7 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 const theme = createMuiTheme({
   typography: {
     fontFamily: "\"Roboto\", \"Helvetica\", \"Arial\", sans-serif",
-    fontSize: 12,
+    fontSize: 14,
     fontWeightLight: 300,
     fontWeightRegular: 400,
     fontWeightMedium: 500,
