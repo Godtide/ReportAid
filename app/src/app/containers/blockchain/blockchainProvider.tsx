048e655c88c7f05af04ec3d8952ef4d57ada7eac
Steve Huckle <s.huckle@sussex.ac.uk>
2018-11-18 18:56:07 +0000

    mapStateToProps

--- app/src/app/containers/blockchain/blockchainProvider.tsx
+++ app/src/app/containers/blockchain/blockchainProvider.tsx
@@ -4,6 +4,20 @@ import { Dispatch } from 'redux'
 import { ApplicationState } from '../../store'
 import { addInfo, addObjects } from '../../store/blockchain/actions'
 
+import { BlockchainProps } from '../../store/blockchain/types'
+
+const mapStateToProps = (state: ApplicationState): BlockchainProps => {
+  return {
+    APIName: state.blockchain.APIName,
+    networkName: state.blockchain.networkName,
+    networkChainId: state.blockchain.networkChainId,
+    networkENSAddress: state.blockchain.networkENSAddress,
+    account: state.blockchain.account,
+    web3: state.blockchain.web3,
+    ethers: state.blockchain.ethers
+  }
+}
+
 const mapDispatchToProps = (dispatch: Dispatch, ownProps: any): any => {
   return({
     addInfo: () => {dispatch(addInfo(ownProps))},
@@ -11,7 +25,7 @@ const mapDispatchToProps = (dispatch: Dispatch, ownProps: any): any => {
   })
 }
 
-export const SetProvider = connect<void, any, void, ApplicationState>(
-    null, mapDispatchToProps)(
+export const SetProvider = connect<BlockchainProps, any, any, ApplicationState>(
+    mapStateToProps, mapDispatchToProps)(
     setProvider
 )
