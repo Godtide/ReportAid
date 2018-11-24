b63c6076d3c998135a1c5732151a44b7f783e95f
Steve Huckle <s.huckle@sussex.ac.uk>
2018-11-24 16:48:30 +0000

    Reorg'

--- app/src/app/components/blockchain/organisationWriter.tsx
+++ app/src/app/components/blockchain/organisationWriter.tsx
@@ -5,12 +5,14 @@ import { OrganisationProps } from '../../store/IATIWriter/organisationWriter/typ
 import {LinearProgress} from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import {TextField} from 'formik-material-ui'
+import { WithStyles } from '@material-ui/core/styles/withStyles'
+import { styles } from '../../styles/theme'
 
-export interface DispatchProps {
+export interface OrgDispatchProps {
   handleSubmit: (values: any) => void
 }
 
-type AllProps = OrganisationProps & DispatchProps
+type OrgFormProps = OrganisationProps & OrgDispatchProps
 
 const validate = (values: OrganisationProps): Partial<OrganisationProps> => {
   const errors: Partial<OrganisationProps> = {}
@@ -26,7 +28,7 @@ const validate = (values: OrganisationProps): Partial<OrganisationProps> => {
   return errors
 }
 
-export const OrgForm: React.SFC<AllProps> = (props: AllProps) => {
+const OrgForm: React.SFC<OrgFormProps> = (props: OrgFormProps) => {
   return (
     <div>
       <Formik
@@ -68,6 +70,23 @@ export const OrgForm: React.SFC<AllProps> = (props: AllProps) => {
             </Button>
           </Form>
         )}
+      />
+    </div>
+  )
+}
+
+type OrgWriterProps = WithStyles<typeof styles> & OrgDispatchProps
+
+export const OrgWriter: React.SFC<OrgWriterProps> = (props: OrgWriterProps) => {
+
+  return (
+    <div>
+      <h2>{Organisation.headingOrgWriter}</h2>
+      <OrgForm
+        handleSubmit={(values: any) => props.handleSubmit(values)}
+        name = ''
+        reference = ''
+        type = ''
       />
     </div>
   )
--- app/src/app/containers/pages/IATIWriter/orgWriter.tsx
+++ app/src/app/containers/pages/IATIWriter/orgWriter.tsx
@@ -1,35 +1,13 @@
-import * as React from 'react'
 import { connect } from 'react-redux'
 import { Dispatch, AnyAction } from 'redux'
-/*import { ConfigProps } from 'redux-form'
-import { ApplicationState } from '../../../store'*/
 
-import { Organisation } from '../../../utils/strings'
-
-import { OrgForm, DispatchProps } from '../../../components/io/organisationForm'
+import { OrgWriter, OrgDispatchProps } from '../../../components/blockchain/organisationWriter'
 import { addOrganisation } from '../../../store/IATIWriter/organisationWriter/actions'
 
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles'
+import withStyles from '@material-ui/core/styles/withStyles'
 import { withTheme, styles } from '../../../styles/theme'
 
-type AllProps = WithStyles<typeof styles> & DispatchProps
-
-const Org: React.SFC<AllProps> = (props: AllProps) => {
-
-  return (
-    <div>
-      <h2>{Organisation.headingOrgWriter}</h2>
-      <OrgForm
-        handleSubmit={(values: any) => props.handleSubmit(values)}
-        name = ''
-        reference = ''
-        type = ''
-      />
-    </div>
-  )
-}
-
-function mapDispatchToProps(dispatch: Dispatch<AnyAction>): DispatchProps {
+function mapDispatchToProps(dispatch: Dispatch<AnyAction>): OrgDispatchProps {
   return {
     handleSubmit: (ownProps: any) => dispatch(addOrganisation(ownProps))
   }
@@ -38,4 +16,4 @@ function mapDispatchToProps(dispatch: Dispatch<AnyAction>): DispatchProps {
 export const OrganisationForm = withTheme(withStyles(styles)(connect(
   null,
   mapDispatchToProps
-)(Org)))
+)(OrgWriter)))
