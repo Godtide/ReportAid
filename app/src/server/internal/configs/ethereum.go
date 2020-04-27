47ce8301155d9d457c75ead768a16e657754f610
Steve Huckle <steve.huckle@gmail.com>
2020-04-27 22:13:07 +0100

    Contract address

--- app/src/server/internal/configs/ethereum.go
+++ app/src/server/internal/configs/ethereum.go
@@ -3,7 +3,6 @@ package configs
 const (
     // AddrRinkeby - Rinkeby address + infura key
     AddrRinkeby             = "https://rinkeby.infura.io/v3/f4851ba4a9e546e7a1f16bf8e14f0a92"
-    // AddrActivitiesContract - activities contract address + infura key
-    //ActivitiesContractAddr EtherConfig     = "0x1b61bE4D8f631afE6F24F595Df73D5275050B231"
-    AddrActivitiesContract  = "0x1b61bE4D8f631afE6F24F595Df73D5275050B232"
+    // AddrActivitiesContract - activities contract address
+    AddrActivitiesContract  = "0x1b61bE4D8f631afE6F24F595Df73D5275050B231"
 )
