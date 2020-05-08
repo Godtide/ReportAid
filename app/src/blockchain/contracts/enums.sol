76c1c53c147bd201846dbc9e5d3d02b6402b545b
Steve Huckle <steve.huckle@gmail.com>
2020-05-08 17:57:48 +0100

    moving the solidity code forward

---
+++ app/src/blockchain/contracts/enums.sol
@@ -0,0 +1,162 @@
+enum Scope {
+    NONE,
+    GLOBAL,
+    REGIONAL,
+    MULTINATIONAL,
+    NATIONAL,
+    SUBNATIONALMULTIFIRSTLEVEL,
+    SUBNATIONALSINGLEFIRSTLEVEL,
+    SUBNATIONALSINGLESECONDLEVEL,
+    SINGLELOCATION,
+    MAX
+}
+
+enum CollaborationType {
+    NONE,
+    BILATERAL,
+    MULTILATERALINFLOWS,
+    BILATERALPRIVATE,
+    MULTILATERALOUTFLOWS,
+    PRIVATEOUTFLOWS,
+    BILATERALCOREFUNDED,
+    TRIANGULAR,
+    MAX
+}
+
+enum TiedStatus {
+    UNSPECIFIED,
+    UNSPECIFIEDONE,
+    NONE,
+    PARTIALLYTIED,
+    TIED,
+    UNTIED,
+    MAX
+}
+
+enum DateCodes {
+    NONE,
+    PLANNEDSTART,
+    ACTUALSTART,
+    PLANNEDEND,
+    ACTUALEND,
+    MAX
+}
+
+enum Role {
+    NONE,
+    FUNDING,
+    ACCOUNTABLE,
+    EXTENDING,
+    IMPLEMENTING,
+    MAX
+}
+
+enum RelationType {
+    NONE,
+    PARENT,
+    CHILD,
+    SIBLING,
+    COFUNDED,
+    THIRDPARTY,
+    MAX
+}
+
+enum ActivityHierarchy {
+    NONE,
+    ACTIVITY,
+    SUBACTIVITY,
+    SUBSUBACTIVITY,
+    MAX
+}
+
+enum ActivityStatus {
+    NONE,
+    PIPELINEIDENTIFICATION,
+    IMPLEMENTATION,
+    FINALISATION,
+    CLOSED,
+    CANCELLED,
+    SUSPENDED,
+    MAX
+}
+
+enum BudgetNotProvided {
+    NONE,
+    COMMERCIALRESTRICTIONS,
+    LEGALRESTRICTIONS,
+    RAPIDONSETEMERGENCY,
+    MAX
+}
+
+enum TransactionType {
+    NONE,
+    INCOMINGFUNDS,
+    OUTGOINGCOMMITMENT,
+    DISBURSEMENT,
+    EXPENDITURE,
+    INTERESTPAYMENT,
+    LOANREPAYMENT,
+    REIMBURSEMENT,
+    PURCHASEOFEQUITY,
+    SALEOFEQUITY,
+    CREDITGUARANTEE,
+    INCOMINGCOMMITMENT,
+    OUTGOINGPLEDGE,
+    INCOMINGPLEDGE,
+    MAX
+}
+
+enum DisbursementChannel {
+    NONE,
+    TREASURY,
+    BANKACCOUNT,
+    NGO,
+    DONORMANAGED,
+    MAX
+}
+
+enum TiedStatus {
+    UNSPECIFIED,
+    UNSPECIFIEDONE,
+    NONE,
+    PARTIALLYTIED,
+    TIED,
+    UNTIED,
+    MAX
+}
+
+enum BudgetOwner {
+    NONE,
+    ORGEXPENDITURE,
+    ORG,
+    ORGRECIPIENT,
+    ORGREGION,
+    ORGCOUNTRY,
+    ACTIVITY,
+    MAX
+}
+
+enum BudgetStatus {
+    NONE,
+    INDICATIVE,
+    COMMITTED,
+    MAX
+}
+
+enum BudgetType {
+    NONE,
+    ORIGINAL,
+    REVISED,
+    MAX
+}
+
+enum DocAttributes {
+    TITLE,
+    FORMAT,
+    URL,
+    CATEGORY,
+    COUNTRYCODE,
+    DESC,
+    LANG,
+    DATE
+}
---
+++ app/src/blockchain/contracts/storeage.sol
@@ -0,0 +1,137 @@
+struct Activities {
+    bytes32 version;
+    bytes32 generatedTime;
+    bytes32 linkedData;
+}
+
+struct Additional {
+    uint8 scope;
+    uint8 capitalSpend;
+    uint8 collaborationType;
+    uint8 defaultFlowType;
+    uint8 defaultTiedStatus;
+    bytes32 defaultAidType;
+    uint256 defaultFinanceType;
+}
+
+struct Date {
+    uint8 dateType;
+    bytes32 date;
+    string narrative;
+}
+
+struct ParticipatingOrg {
+    uint8 orgType;
+    uint8 role;
+    uint256 crsChannelCode;
+    bytes32 lang;
+    bytes32 orgRef;
+    string narrative;
+}
+
+struct RelatedActivity {
+    uint8 relationType;
+    bytes32 activityRef;
+}
+
+struct Sector {
+    uint8 percentage;
+    uint256 dacCode;
+}
+
+struct ReportingOrg {
+    uint8 orgType;
+    bool isSecondary;
+    bytes32 orgRef;
+}
+
+struct Activity {
+    bool humanitarian;
+    uint8 hierarchy;
+    uint8 status;
+    uint8 budgetNotProvided;
+    ReportingOrg reportingOrg;
+    bytes32 lastUpdated;
+    bytes32 lang;
+    bytes32 currency;
+    bytes32 linkedData;
+    bytes32 identifier;
+    string title;
+    string description;
+}
+
+struct Territory {
+    uint8 percentage;
+    bytes32 territory;
+}
+
+struct Value {
+    uint256 value;
+    bytes32 date;
+    bytes32 currency;
+}
+
+struct TransactionOrg {
+    uint8 orgType;
+    bytes32 orgRef;
+    bytes32 activityIdentifier;
+}
+
+struct Transaction {
+    uint8 transactionType;
+    uint8 disbursementChannel;
+    uint8 flowType;
+    uint8 tiedStatus;
+    TransactionOrg providerOrg;
+    TransactionOrg receiverOrg;
+    Value value;
+    uint256 financeType;
+    bytes32 aidType;
+    bytes32 date;
+    uint256 sectorDacCode;
+    bytes32 territory;
+    string description;
+}
+
+struct Finance {
+    uint8 status;
+    uint256 value;
+    bytes32 start;
+    bytes32 end;
+}
+
+struct Budget {
+    uint8 budgetType;
+    bytes32 budgetLine;
+    bytes32 otherRef;
+    Finance finance;
+}
+
+struct Document {
+    bytes32 category;
+    bytes32 countryRef;
+    bytes32 lang;
+    bytes32 date;
+    string desc;
+    string title;
+    string format;
+    string url;
+}
+
+struct Organisation {
+    ReportingOrg reportingOrg;
+    bytes32 orgRef;
+    bytes32 lang;
+    bytes32 currency;
+    bytes32 lastUpdatedTime;
+}
+
+struct Orgs {
+    bytes32 version;
+    bytes32 generatedTime;
+}
+
+struct Org {
+    string name;
+    string identifier;
+}
